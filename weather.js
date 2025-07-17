df4223bb9eb278cedda9681530dc81c812ef7d4c
G384282023 <238428@st.takushoku-u.ac.jp>
2025-07-17 14:23:25 +0900

    第六回課題途中

--- weather.js
+++ weather.js
@@ -43,16 +43,32 @@ function printDom(data) {
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
+let b = document.querySelector('#sendRequest');
+b.addEventListener('click', sendRequest);
 
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/{id}.json';
 
+    axios.get(url)
+        .then(showResult)   
+        .catch(showError)   
+        .then(finish);     
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  let data = resp.data;
+
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    console.log(data);
+
+    console.log(data.x);
 
 }
 
