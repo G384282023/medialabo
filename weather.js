6c315c7b455fe6d62ca3fc9e71ba5c2192e3b9b3
G384282023 <238428@st.takushoku-u.ac.jp>
2025-07-18 14:38:15 +0900

    project修正

--- weather.js
+++ weather.js
@@ -19,23 +19,62 @@ function print(data) {
 
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
+  let old = document.querySelector('div#result');
+  if (old !== null) {
+    old.remove();
+  }
   let resultDiv = document.createElement("div");
-  resultDiv.id = "result";
+  resultDiv.setAttribute('id', 'result');
+  let body = document.querySelector('body'); 
+  body.insertAdjacentElement('beforeend', resultDiv);
+  let pop = document.querySelector('div#result');
 
-  resultDiv.innerHTML = `
-    <p>都市名: ${data.name}</p>
-    <p>経度: ${data.coord.lon}</p>
-    <p>緯度: ${data.coord.lat}</p>
-    <p>天気: ${data.weather[0].description}</p>
-    <p>最低気温: ${data.main.temp_min}</p>
-    <p>最高気温: ${data.main.temp_max}</p>
-    <p>湿度: ${data.main.humidity}</p>
-    <p>風速: ${data.wind.speed}</p>
-    <p>風向: ${data.wind.deg}</p>
-  `;
+  let a1 = document.createElement('p');
+  a1.textContent = "都市: " + data.name;
+  pop.appendChild(a1);
 
-  document.body.appendChild(resultDiv);
+  let a2 = document.createElement('p');
+  a2.textContent = "天気: " + data.weather[0].main;
+  pop.appendChild(a2);
+
+  let a3 = document.createElement('p');
+  a3.textContent = "天気の詳細: " + data.weather[0].description;
+  pop.appendChild(a3);
+
+  let a5 = document.createElement('p');
+  a5.textContent = "現在の気温: " + data.main.temp + "℃";
+  pop.appendChild(a5);
+
+  let a6 = document.createElement('p');
+  a6.textContent = "体感温度: " + data.main.feels_like + "℃";
+  pop.appendChild(a6);
+
+  let a7 = document.createElement('p');
+  a7.textContent = "最低気温: " + data.main.temp_min + "℃";
+  pop.appendChild(a7);
+
+  let a8 = document.createElement('p');
+  a8.textContent = "最高気温: " + data.main.temp_max + "℃";
+  pop.appendChild(a8);
+
+  let a9 = document.createElement('p');
+  a9.textContent = "湿度: " + data.main.humidity + "%";
+  pop.appendChild(a9);
+
+  let a10 = document.createElement('p');
+  a10.textContent = "気圧: " + data.main.pressure + "hPa";
+  pop.appendChild(a10);
+
+  let a11 = document.createElement('p');
+  a11.textContent = "風速: " + data.wind.speed + "m/s";
+  pop.appendChild(a11);
+
+  let a12 = document.createElement('p');
+  a12.textContent = "風向: " + data.wind.deg + "°";
+  pop.appendChild(a12);
 }
+
+  
 
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
@@ -108,50 +147,50 @@ function finish() {
 // 以下はグルメのデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
 // 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
+// let data = {
+//   "coord": {
+//     "lon": 116.3972,
+//     "lat": 39.9075
+//   },
+//   "weather": [
+//     {
+//       "id": 803,
+//       "main": "Clouds",
+//       "description": "曇りがち",
+//       "icon": "04d"
+//     }
+//   ],
+//   "base": "stations",
+//   "main": {
+//     "temp": 9.94,
+//     "feels_like": 8.65,
+//     "temp_min": 9.94,
+//     "temp_max": 9.94,
+//     "pressure": 1022,
+//     "humidity": 14,
+//     "sea_level": 1022,
+//     "grnd_level": 1016
+//   },
+//   "visibility": 10000,
+//   "wind": {
+//     "speed": 2.65,
+//     "deg": 197,
+//     "gust": 4.84
+//   },
+//   "clouds": {
+//     "all": 53
+//   },
+//   "dt": 1646542386,
+//   "sys": {
+//     "type": 1,
+//     "id": 9609,
+//     "country": "CN",
+//     "sunrise": 1646520066,
+//     "sunset": 1646561447
+//   },
+//   "timezone": 28800,
+//   "id": 1816670,
+//   "name": "北京市",
+//   "cod": 200
+// };
 
